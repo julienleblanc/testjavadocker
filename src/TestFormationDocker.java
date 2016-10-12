63bdccc808a393b4dec5dac9fac5d9be93074504
julienleblanc <julien.leblanc@atos.net>
2016-10-12 14:32:22 +0000

    2eme commit

--- src/TestFormationDocker.java
+++ src/TestFormationDocker.java
@@ -1,5 +1,5 @@
 public class TestFormationDocker {
 	public static void main(String[] args){
-		System.out.println("Docker c'est cool");
+		System.out.println("Docker c'est cool et git je galère");
 	}
 }
